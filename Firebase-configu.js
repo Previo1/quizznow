159f4d6933e9088e42e77fea033b1a15e829624a
Previo1 <rudakiyapiyush1@gmail.com>
2025-08-01 16:36:21 +0530

    Firebase-configu.js

--- Firebase-configu.js
+++ Firebase-configu.js
@@ -1,15 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBwaAAfND1dBnr_un2qA3YAy0Ipcpsl7JM",
-  authDomain: "newquiz-4454f.firebaseapp.com",
-  projectId: "newquiz-4454f",
-  storageBucket: "newquiz-4454f.firebasestorage.app",
-  messagingSenderId: "268848107378",
-  appId: "1:268848107378:web:111595fbc275823ec5d7eb",
-  measurementId: "G-RYH64757WM"
+  apiKey: "AIzaSyBdVEhGTqBmHjS6NSnJ1FC26Ns1wFX0NcA",
+  authDomain: "nowquiz-80c34.firebaseapp.com",
+  projectId: "nowquiz-80c34",
+  storageBucket: "nowquiz-80c34.firebasestorage.app",
+  messagingSenderId: "628365724942",
+  appId: "1:628365724942:web:1e0db52f7fc6deb19cdb5f",
+  measurementId: "G-8BGT3F01N6"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
